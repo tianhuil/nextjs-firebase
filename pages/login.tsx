1e0aa8ecadb2cb1e42ff481eff6fe2c1b5b8ab89
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2022-04-26 10:22:55 -0400

    save login to localstorage

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,7 +1,8 @@
-import { GoogleAuthProvider, signOut } from "firebase/auth";
+import { Auth, GoogleAuthProvider, signOut as _signOut } from "firebase/auth";
 import type { NextPage } from "next";
 import React from "react";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import superjson from "superjson";
 import { auth } from "../comp/auth";
 
 export const defaultUiConfig = {
@@ -12,8 +13,30 @@ export const defaultUiConfig = {
   signInOptions: [GoogleAuthProvider.PROVIDER_ID],
 };
 
+const useAuth = (auth: Auth) => {
+  const [signIn, _user, loading, error] = useSignInWithGoogle(auth);
+  const getUser = (): typeof _user => {
+    if (typeof window === "undefined") return _user;
+    const __userStr = window.localStorage.getItem("__user");
+    if (!__userStr) return _user;
+    return superjson.parse(__userStr);
+  };
+  const signOut = () => {
+    typeof window !== "undefined" && window.localStorage.removeItem("__user");
+    _signOut(auth);
+  };
+  if (
+    _user !== undefined &&
+    typeof window !== "undefined" &&
+    !window.localStorage.getItem("__user")
+  ) {
+    window.localStorage.setItem("__user", superjson.stringify(_user));
+  }
+  return [signIn, getUser(), loading, error, signOut] as const;
+};
+
 const Login: NextPage = () => {
-  const [signIn, user, loading, error] = useSignInWithGoogle(auth);
+  const [signIn, user, loading, error, signOut] = useAuth(auth);
 
   const display = () => {
     if (loading) return <p>Loading ...</p>;
@@ -22,8 +45,7 @@ const Login: NextPage = () => {
       return (
         <p>
           Logged In as {user.user.displayName} ({user.user.email}) via{" "}
-          {user.providerId}{" "}
-          <button onClick={() => signOut(auth)}>Signout</button>
+          {user.providerId} <button onClick={() => signOut()}>Signout</button>
         </p>
       );
     return (
